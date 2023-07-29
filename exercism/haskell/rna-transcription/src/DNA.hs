af2cf88df395c04ef6785840e2496ad88de26c93
gantonopoulos <georanto@gmail.com>
2023-07-29 18:27:35 +0200

    Implements the first solution to the kata
    I use guards in conjunction with tail recursion to be able to break the
    execution and return an invalid character if it pops up. If it doesn't
    the expression's value gets evaluated after the last character.

--- exercism/haskell/rna-transcription/src/DNA.hs
+++ exercism/haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,13 @@
 module DNA (toRNA) where
 
+decideComplement :: String -> String -> Either Char String
+decideComplement [] tail = Right tail
+decideComplement (x:xs) tail
+    | x == 'G' =  decideComplement xs (tail++"C")
+    | x == 'C' =  decideComplement xs (tail++"G")
+    | x == 'T' =  decideComplement xs (tail++"A")
+    | x == 'A' =  decideComplement xs (tail++"U")
+    | otherwise = Left x
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA xs = decideComplement xs ""
