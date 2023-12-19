58a24cc6d27405ae9c24e06ca5cdc9558f5572b0
gantonopoulos <georanto@gmail.com>
2023-12-19 09:36:07 +0100

    Implements the kata (1st submission)

--- exercism/haskell/nucleotide-count/src/DNA.hs
+++ exercism/haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,19 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+module DNA (interpretNucleotide, nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Data.Map (Map, insertWith, empty)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+interpretNucleotide::String -> (Map Nucleotide Int) -> Either String (Map Nucleotide Int)
+interpretNucleotide [] map = Right map
+interpretNucleotide (element:xs) map
+    | element == 'A' = interpretNucleotide xs (insertWith (+) A 1 map)
+    | element == 'C' = interpretNucleotide xs (insertWith (+) C 1 map)
+    | element == 'G' = interpretNucleotide xs (insertWith (+) G 1 map)
+    | element == 'T' = interpretNucleotide xs (insertWith (+) T 1 map)
+    | otherwise = Left "Error"
+
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+
+nucleotideCounts xs = interpretNucleotide xs empty
