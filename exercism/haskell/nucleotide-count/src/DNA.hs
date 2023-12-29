d32ddd755fc518578c4540cfe3136337598fbfb0
gantonopoulos <georanto@gmail.com>
2023-12-29 09:54:23 +0200

    Implements code review suggestion
    Breaks the one-function implementation into multiple.
    One function to parse the input
    One function to decide a character into a Nucleotide
    One function to count the instances of each nucleodite

--- exercism/haskell/nucleotide-count/src/DNA.hs
+++ exercism/haskell/nucleotide-count/src/DNA.hs
@@ -1,19 +1,29 @@
-module DNA (interpretNucleotide, nucleotideCounts, Nucleotide(..)) where
+module DNA (
+    read', 
+    nucleotideCounts, 
+    count,
+    parse,
+    Nucleotide(..)) where
 
 import Data.Map (Map, insertWith, empty)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-interpretNucleotide::String -> (Map Nucleotide Int) -> Either String (Map Nucleotide Int)
-interpretNucleotide [] map = Right map
-interpretNucleotide (element:xs) map
-    | element == 'A' = interpretNucleotide xs (insertWith (+) A 1 map)
-    | element == 'C' = interpretNucleotide xs (insertWith (+) C 1 map)
-    | element == 'G' = interpretNucleotide xs (insertWith (+) G 1 map)
-    | element == 'T' = interpretNucleotide xs (insertWith (+) T 1 map)
+count::[Nucleotide]->Map Nucleotide Int
+count [] = empty
+count nucleotides = foldr(\element acc->insertWith (+) element 1 acc) empty nucleotides
+
+read'::Char->Either String Nucleotide
+read' element 
+    | element == 'A' = Right A
+    | element == 'C' = Right C
+    | element == 'G' = Right G
+    | element == 'T' = Right T
     | otherwise = Left "Error"
 
 
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+parse::String -> Either String [Nucleotide]
+parse = traverse read'
 
-nucleotideCounts xs = interpretNucleotide xs empty
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts elements =  fmap count $ parse elements
