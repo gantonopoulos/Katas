51951fce45bf7bf894e670aa2245c785bbf9af69
gantonopoulos <georanto@gmail.com>
2024-01-02 18:06:37 +0200

    Computes multiples of a number up to limit
    Implement a function that computes the multiples of a given number below
    a given limit.

--- exercism/haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ exercism/haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,12 @@
-module SumOfMultiples (sumOfMultiples) where
+module SumOfMultiples (
+    sumOfMultiples,
+    multiplesOfFactor) where
+
+multiplesOfFactor :: Integer -> Integer ->[Integer]
+multiplesOfFactor factor limit 
+    | factor < 0  = []
+    | limit <= factor = []
+    | otherwise = factor : fmap (+factor) (multiplesOfFactor factor (limit-factor))
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = 0
